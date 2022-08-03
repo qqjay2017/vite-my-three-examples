06898993862c9c583ed7619546e9955ca285b836
Boris <qqjay2013@gmail.com>
2022-08-03 23:07:40 +0800

    feat: 添加注释-11

--- src/examples/mesh_base/lib/scene.ts
+++ src/examples/mesh_base/lib/scene.ts
@@ -3,7 +3,8 @@ import * as THREE from "three";
 export function getScene() {
   const scene = new THREE.Scene();
   const textureCubeLoader = new THREE.CubeTextureLoader()
-
+  // 设置默认路径
+  // textureCubeLoader.setPath()
  const textureCube =  textureCubeLoader.load([
     './textures/env/1.jpg',
     './textures/env/2.jpg',
